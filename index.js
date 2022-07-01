9e7a5a15e74c71d9ff332fdbfaefb04043372c77
Adam <adam.l.sosa@gmail.com>
2022-07-01 14:57:08 -0400

    commit tree 42f9d292560057c49b2eebbc2561ee609953d263

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+
+
+function writeCards(names, event){
+    const emptyArray = [];
+    //1. create a new array of the function
+    //2. inside the for loop, do something to the array
+    //3. return the array 
+    for(let i = 0; i < names.length; i++) {
+        emptyArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return emptyArray;
+}
+
+function countDown() {
+    let countDown = 10;
+    while (countDown >= 0) {
+      console.log(countDown--);
+    } }
